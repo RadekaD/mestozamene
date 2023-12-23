fc7f2a10370663d7cd1442cd909acc26c4733c75
po-danilo-radeka <121094729+po-danilo-radeka@users.noreply.github.com>
2023-12-23 09:33:14 +0100

    Working on Hero section

--- src/pages/manifest.json.js
+++ src/pages/manifest.json.js
@@ -1,35 +1,35 @@
-import { getImage } from "astro:assets";
-import favicon from "../images/favicon.png";
+// import { getImage } from "astro:assets";
+// import favicon from "../images/favicon.png";
 
-const faviconPngSizes = [192, 512];
+// const faviconPngSizes = [192, 512];
 
-export const GET = async () => {
-  const icons = await Promise.all(
-    faviconPngSizes.map(async (size) => {
-      const image = await getImage({
-        src: favicon,
-        width: size,
-        height: size,
-        format: "png",
-      });
-      return {
-        src: image.src,
-        type: `image/${image.options.format}`,
-        sizes: `${image.options.width}x${image.options.height}`,
-      };
-    })
-  );
+// export const GET = async () => {
+//   const icons = await Promise.all(
+//     faviconPngSizes.map(async (size) => {
+//       const image = await getImage({
+//         src: favicon,
+//         width: size,
+//         height: size,
+//         format: "png",
+//       });
+//       return {
+//         src: image.src,
+//         type: `image/${image.options.format}`,
+//         sizes: `${image.options.width}x${image.options.height}`,
+//       };
+//     })
+//   );
 
-  const manifest = {
-    name: "Mesto za Mene | Podrška pri neplaniranoj trudnoći",
-    short_name: "Mesto za Mene",
-    description:
-      "Mesto za mene je besplatno savetovalište za žene koje se suočavaju sa neplaniranom i/ili kriznom trudnoćom i nastalo je po ugledu na centre koji postoje širom sveta (Pregnancy Help Centers).",
-    start_url: "/",
-    display: "standalone",
-    id: "mesto-za-mene",
-    icons,
-  };
+//   const manifest = {
+//     name: "Mesto za Mene | Podrška pri neplaniranoj trudnoći",
+//     short_name: "Mesto za Mene",
+//     description:
+//       "Mesto za mene je besplatno savetovalište za žene koje se suočavaju sa neplaniranom i/ili kriznom trudnoćom i nastalo je po ugledu na centre koji postoje širom sveta (Pregnancy Help Centers).",
+//     start_url: "/",
+//     display: "standalone",
+//     id: "mesto-za-mene",
+//     icons,
+//   };
 
-  return new Response(JSON.stringify(manifest))
-};
+//   return new Response(JSON.stringify(manifest))
+// };
